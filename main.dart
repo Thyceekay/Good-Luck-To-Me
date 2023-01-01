bd939b8d651c14a5092b9fa678854e23bc5551fd
Hassan Eleburuike <eleburuikehassan@gmail.com>
2023-01-01 23:38:26 +0100

    My second project

--- main.dart
+++ main.dart
@@ -1,14 +1,53 @@
 
-import 'dart:io';
+bool stringValidator(String stringValid) {
+  if(
+    stringValid.length >=6 
+    && stringValid.length <=15 
+    && stringValid.contains(RegExp(r'[A-Z]')) 
+    && stringValid.contains(RegExp(r'[a-z]')) 
+    && stringValid.contains(RegExp(r'[>$%0-9-]'))
+    && stringValid.contains(RegExp(r'[0-9-]'))
+    )
+   {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
 
-void stringValidator(String Count1, String Count2) {
-  print('$Count1 with $Count2');
-  bool isEqual = Count1 == Count2;
-  print(isEqual);
-  bool isEqual1 = Count1 != Count2;
-  print(isEqual1);
-}
+
 void main() {
-  stringValidator("1gH2>", "Lit3b4%");
+  print('Whatever it is to be printed must contain at least a lowercase and a uppercase');
+  print('And must contain a minimum of 6 and maximum of 15 characters');
+  print('And must contain at least a number and a character from the following list: [%, >, and the dollar sign]');
+  print(stringValidator("StrIng%4"));
+  print(stringValidator("two"));
+
+  
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
